f0ec8c4b45e635867c661a5de5c4e981cbe5c434
JJNunez03 <112155908+JJNunez03@users.noreply.github.com>
2024-05-04 06:43:49 +0000

    3 STACKS ASSIGNMENT

--- 3Stacks.cc
+++ 3Stacks.cc
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
+#include <stack>
 
+using namespace std;
+
+bool checkBalancedParentheses(const string& expression) {
+    stack<string> parenthesesStack;
+    for (char c : expression) {
+        if (c == '(') {
+            parenthesesStack.push("(");
+        } else if (c == ')') {
+            if (parenthesesStack.empty() || parenthesesStack.top() != "(") {
+                return false; 
+            }
+            parenthesesStack.pop();
+        }
+    }
+    return parenthesesStack.empty();
+}
+
+int main() {
+    string expression;
+    cout << "Enter an expression: ";
+    cin >> expression;
+
+    if (checkBalancedParentheses(expression)) {
+        cout << "Balanced\n";
+    } else {
+        cout << "Not Balanced\n";
+    }
+
+    return 0;
+}
